683ac1990bd9ad7aa0c980a8c5f9bbb608ea6187
Jason Lengstorf <jason@lengstorf.com>
2021-02-22 12:28:09 -0800

    wip: clean up logging

--- helpers/getContent.js
+++ helpers/getContent.js
@@ -1,32 +1,9 @@
 import fs from 'fs';
 import path from 'path';
-import execa from 'execa';
 
-function getDataRoot() {
-  // if (process.env.AWS_LAMBDA_FUNCTION_VERSION) {
-  //   console.log({
-  //     __dirname,
-  //     path: path.resolve('./_content'),
-  //     modulePath: module.path,
-  //     'process.env.LAMBDA_TASK_ROOT': process.env.LAMBDA_TASK_ROOT,
-  //     'process.env.LAMBDA_RUNTIME_DIR': process.env.LAMBDA_RUNTIME_DIR,
-  //     global: global,
-  //     __filename,
-  //     'require.main.filename': require.main.filename,
-  //     // 'import.meta.url': import.meta.url,
-  //   });
-  //   return path
-  //     .resolve('./_content')
-  //     .replace('/var/task', '/var/task/src/out_functions/next_slug');
-  // } else {
-  return path.resolve(process.cwd(), '_content');
-  // }
-}
-
-const dataRoot = getDataRoot();
+const dataRoot = path.resolve(process.cwd(), '_content');
 
 const getContent = (slug) => {
-  console.log('HELLO', process.cwd(), __dirname, __filename);
   let content = fs.readFileSync(`${dataRoot}/${slug}.md`, 'utf8');
 
   if (!content) {
